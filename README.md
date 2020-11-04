0de343e1d21664a33f88b91ff590da18b1ee73cf
matheusc3m <matheusfernandes3m@hotmail.com>
2020-11-04 18:44:02 -0300

    App ok

--- README.md
+++ README.md
@@ -1,28 +1,15 @@
-![App Brewery Banner](https://github.com/londonappbrewery/Images/blob/master/AppBreweryBanner.png)
+
 
 # Mi Card
 
-## Our Goal
+## Informações
 
-Now that you've seen how to create a Flutter app entirely from scratch, we're going to go further and learn more about how to design user interfaces for Flutter apps.
+Esse app foi feito acompanhando o bootcamp de Flutter ministrado pela Angela Yu, ela mostrou a imagem de como seria o app, eu pausei o vídeo e fiz antes dela explicar e depois vi a solulção.
 
-## What you will create
+## Imagens do App
 
-Mi Card is a personal business card. Imagine every time you wanted to give someone your contact details or your business card but you didn't have it on you. Well, now you can get them to download your business card as an app.
-
-## What you will learn
-
-* How to create Stateless Widgets
-* What is the difference between hot reload and hot refresh and running an app from cold
-* How to use Containers to lay out your UI
-* How to use Columns and Rows to position your UI elements
-* How to add custom fonts
-* How to add Material icons
-* How to style Text widgets
-* How to read and use Flutter Documentation
+![App](https://i.imgur.com/sejnh05.png)
 
 
 
->This is a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
 
-![End Banner](https://github.com/londonappbrewery/Images/blob/master/readme-end-banner.png)
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,69 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.red,
+                  backgroundImage: NetworkImage(
+                      "https://media-exp1.licdn.com/dms/image/C4D03AQExOhvMufIQgA/profile-displayphoto-shrink_400_400/0?e=1609977600&v=beta&t=5fogp2w1hg52MUG8awu_g-C_ru7Y5kIsX0XqX2NiAUA"),
+                ),
+                Text(
+                  "Matheus Cavalcante",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "PermanentMarker"),
+                ),
+                Text(
+                  "Flutter developer",
+                  style: TextStyle(
+                      color: Colors.white30,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "MeriendaOne"),
+                ),
+                Center(
+                  child: Card(
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: ListTile(
+                      title: Text(
+                        "+55 38 9 8853-0318",
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      leading: Icon(Icons.phone),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Card(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    child: ListTile(
+                      title: Text(
+                        "matheusfernandes3m@hotmail.com",
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      leading: Icon(Icons.email),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
 }
